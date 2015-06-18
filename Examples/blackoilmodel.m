550711b1f8a0558b6642bee7215650b7b2caf019
AAE <e.eftekhari@gmail.com>
2015-06-18 13:57:44 +0200

    small changes to the blackoil example

--- Examples/blackoilmodel.m
+++ Examples/blackoilmodel.m
@@ -7,7 +7,6 @@ z=[0.1, 0.1, 0.1, 0.2, 0.3, 0.2]
 'C6H14', 'C8H18', 'C10H22'});
 % Define the thermodynamic models
 T = 100+273.15; % [K]
-p = 100e5; % [Pa]
 T0= 25+273.15; % [K] surface temperature
 p0= 1e5; % [Pa] surface pressure
 thermo1 = addThermo();
@@ -29,7 +28,10 @@ mixture1.mole_fraction=vapor_y;
 [liquid_z, vapor_z_surf, fugacity, HR] = PREOS(mixture1, thermo1);
 V_gas_surface = vapor_z_surf*vapor_frac_surf*R*T0/p0;
 
-
+n_point=50;
+p_range=linspace(p0, 300e5, n_point);
+for i=1:n_point
+p=p_range(i);
 % flash for reservoir properties
 mixture1.temperature=T;
 mixture1.pressure=p;
@@ -43,17 +45,18 @@ if vapor_frac_res<=0
 end  
 mixture1.mole_fraction=liquid_x;
 [liquid_z_res, vapor_z, fugacity, HR] = PREOS(mixture1, thermo1);
-V_oil_res = liquid_z_res*(1-vapor_frac_res)*R*T0/p0;
+V_oil_res = liquid_z_res*(1-vapor_frac_res)*R*T/p;
 mixture1.mole_fraction=vapor_y;
 [liquid_z, vapor_z_res, fugacity, HR] = PREOS(mixture1, thermo1);
-V_gas_res = vapor_z_res*vapor_frac_res*R*T0/p0;
+V_gas_res = vapor_z_res*vapor_frac_res*R*T/p;
 
 %[s1, sl, sv] = stabilityTest(mixture1, thermo1)
-Bo = V_oil_res/V_oil_surface
-Bg = V_gas_res/V_oil_surface
-Rs = (vapor_frac_surf-vapor_frac_res)*vapor_z_surf*R*T0/p0/V_oil_surface
-rs = (vapor_frac_res-vapor_frac_surf)*liquid_z_surf*R*T0/p0/V_gas_surface;
-if rs<0
-  rs=0;
+Bo(i) = V_oil_res/V_oil_surface
+Bg(i) = V_gas_res/V_oil_surface
+Rs(i) = (vapor_frac_surf-vapor_frac_res)*vapor_z_surf*R*T0/p0/V_oil_surface
+rs1 = (vapor_frac_res-vapor_frac_surf)*liquid_z_surf*R*T0/p0/V_gas_surface;
+if rs1<0
+  rs1=0;
 end
-rs
+rs(i)=rs1;
+end
